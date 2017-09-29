65bd4c81945778f98c2a56ee89df080b69704198
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-29 18:37:56 +0700

    Init autoloader

---
+++ autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__."/config.php";
+
+function ___load_class($class)
+{
+	require __DIR__."/src/".str_replace("\\", "/", $class).".php";
+}
+
+spl_autoload_register("___load_class");

d1fcefc4751e2d95ad2b2c70541af21495e35c3b
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-29 23:58:23 +0700

    Create language map

---
+++ src/Lang/Map.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Lang;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ */
+final class Map
+{
+	/**
+	 * @var array
+	 */
+	public static $map = [
+		"id" => "\\Lang\\ID\\",
+		"en" => "\\Lang\\EN\\"
+	];
+}

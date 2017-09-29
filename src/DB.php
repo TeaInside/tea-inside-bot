3d50812558924209023b7b7291cd5caa9c639ba2
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-29 23:25:57 +0700

    Create DB class

---
+++ src/DB.php
@@ -0,0 +1,37 @@
+<?php
+
+use PDO;
+use Hub\Singleton;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ */
+final class DB
+{
+	use Singleton;
+
+	/**
+	 * @var \PDO
+	 */
+	private $pdo;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		$this->pdo = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+	}
+
+	/**
+	 * Call any PDO method.
+	 * @param 	string $method
+	 * @param 	array  $param
+	 * @return 	mixed
+	 */
+	public static function __callStatic($method, $param)
+	{
+		return self::getInstance()->pdo->{$method}(...$param);
+	}
+}

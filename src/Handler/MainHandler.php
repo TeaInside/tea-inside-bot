e095852dbd451eb512d5a95b1aeaa8dd7f551677
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-29 23:37:20 +0700

    Improve MainHandler

--- src/Handler/MainHandler.php
+++ src/Handler/MainHandler.php
@@ -2,12 +2,24 @@
 
 namespace Handler;
 
+use Handler\Response;
+
 /**
  * @author Ammar Faizi <ammarfaizi2@gmail.com>
  * @license MIT
  */
 final class MainHandler
 {
+	/**
+	 * @var string
+	 */
+	private $chattype;
+
+	/**
+	 * @var string
+	 */
+	private $text;
+
 	/**
 	 * Constructor.
 	 * @param string $input
@@ -18,6 +30,25 @@ final class MainHandler
 			$this->input = json_decode($input, true);
 		} else {
 			$this->input = json_decode(file_get_contents("php://input"), true);
+		}
+	}
+
+	/**
+	 * Run app.
+	 */
+	public function run()
+	{
+		$this->parseEvent();
+	}
+
+	/**
+	 * Parse Webhook Event.
+	 */
+	private function parseEvent()
+	{
+		if (isset($this->input['message']['text'])) {
+			$this->chattype = "text";
+			$this->
 		}
 	}
 }

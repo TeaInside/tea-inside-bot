15876dcc728c454b768c97e4bde4388575c9e9d1
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-30 00:22:02 +0700

    Update autoload

--- autoload.php
+++ autoload.php
@@ -4,7 +4,15 @@ require __DIR__."/config.php";
 
 function ___load_class($class)
 {
-	require __DIR__."/src/".str_replace("\\", "/", $class).".php";
+    require __DIR__."/src/".str_replace("\\", "/", $class).".php";
 }
 
 spl_autoload_register("___load_class");
+
+function pc($exe, $st)
+{
+	if (! $exe) {
+		var_dump($st->errorInfo());
+		die;
+	}
+}

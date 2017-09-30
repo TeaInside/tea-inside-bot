8fba4087cf29b14e2b320546c531f3ec7e189ff5
Ammar Faizi <ammarfaizi2@gmail.com>
2017-09-30 20:27:20 +0700

    Update background pr

--- connector/webhook.php
+++ connector/webhook.php
@@ -1,4 +1,5 @@
 <?php
 
 require __DIR__."/../autoload.php";
-
+$input = urlencode(file_get_contents("php://input"));
+shell_exec("nohup /usr/bin/php cli.php \"".urlencode($input)."\" >> ".LOG_DIR."/nohup.out 2>&1 &");

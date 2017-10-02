fb3760d485a105e4a7b38edbd047733729841025
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-02 21:19:23 +0700

    Init Ruby virtualizor class

---
+++ src/App/Virtualizor/Lang/Ruby.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Virtualizor\Lang;
+
+use System\Contracts\App\Virtualizor\LangContract;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ */
+class Ruby implements LangContract
+{
+}

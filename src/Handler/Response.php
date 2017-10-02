46ececb2d5826f1a7bed7b0f9340e8ebbf13784d
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-02 20:41:32 +0700

    Update CPPHandler

--- src/Handler/Response.php
+++ src/Handler/Response.php
@@ -63,7 +63,7 @@ class Response
 			[
 				"chat_id" 	 => $this->h->chat_id,
 				"text"    	 => $msg,
-				"parse_mode" => "HTML",
+				// "parse_mode" => "HTML",
 				"reply_to_message_id" => $this->h->msgid
 			]
 		);

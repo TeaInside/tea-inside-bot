6c5e7b3b68ebd63fc30268897dfd9ec944c6f7cf
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-02 21:09:03 +0700

    Create NodeJS test case

---
+++ tests/VirtualizorNonCompiler/NodeJSTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace VirtualizorNonCompiler;
+
+use App\Virtualizor\Lang\NodeJS;
+use PHPUnit\Framework\TestCase;
+
+class NodeJSTest extends TestCase
+{
+	public function test1()
+	{
+		$code = " console.log(\"hello world\");";
+		$aa = new NodeJS($code);
+		$this->assertTrue(trim($aa->exec()) == "hello world");
+	}
+}

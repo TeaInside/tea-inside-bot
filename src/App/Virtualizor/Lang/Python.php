42b95e949864961aeb7b390e8fdd70f059c88ea7
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-03 14:02:41 +0700

    Improve Python virtualizor class

--- src/App/Virtualizor/Lang/Python.php
+++ src/App/Virtualizor/Lang/Python.php
@@ -10,4 +10,38 @@ use System\Contracts\App\Virtualizor\LangContract;
  */
 class Python implements LangContract
 {
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $pyCode
+	 */
+	public function __construct($pyCode)
+	{
+		$this->pyCode = $pyCode;
+		$this->hash   = sha1($pyCode);
+		$this->file   = VIRTUALIZOR_DIR."/python/".$this->hash.".py";
+	}
+
+	private function __init()
+	{
+		if (! is_dir(VIRTUALIZOR_DIR."/python")) {
+			$exe = shell_exec("mkdir -p ".VIRTUALIZOR_DIR."/python");
+			if (! is_dir(VIRTUALIZOR_DIR."/python")) {
+				throw new \Exception($exe, 1);
+			}
+		}
+
+		if (! file_exists($this->file)) {
+			$handle = fopen($this->file, "w");
+			fwrite($handle, $this->pyCode);
+			fclose($handle);
+		}
+	}
+
+	public function exec()
+	{
+		$sh = shell_exec("python ".$this->file." 2>&1");
+		return empty($sh) ? "~" : $sh;
+	}
 }

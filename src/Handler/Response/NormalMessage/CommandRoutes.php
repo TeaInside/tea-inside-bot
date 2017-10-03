f59a82c702a3be2346b55132c0544a03335ad340
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-03 14:43:52 +0700

    Update command handler

--- src/Handler/Response/NormalMessage/CommandRoutes.php
+++ src/Handler/Response/NormalMessage/CommandRoutes.php
@@ -2,9 +2,43 @@
 
 namespace Handler\Response\NormalMessage;
 
+use Handler\MainHandler;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ */
 class CommandRoutes
 {
-	public function route()
+	/**
+	 * @var Handler\MainHandler
+	 */
+	private $h;
+
+	/**
+	 * @var array
+	 */
+	private $routes = [];
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Handler\MainHandler
+	 */	
+	public function __construct(MainHandler $handler)
+	{
+		$this->h = $handler;
+	}
+
+	private function routes()
+	{
+		$this->routes = [
+
+
+		];
+	}
+
+	public function needResponse()
 	{
 
 	}

ebc61f0dc5b617f5b69bc873fe874871b91021f6
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-05 20:04:02 +0700

    Fix creator username

--- src/Handler/Response/NormalMessage/Command/Admin.php
+++ src/Handler/Response/NormalMessage/Command/Admin.php
@@ -36,7 +36,7 @@ class Admin extends CommandFactory
 		foreach ($a['result'] as $val) {
 			$name = $val['user']['first_name']. (isset($val['user']['last_name']) ? " ".$val['user']['last_name'] : "");
 			if ($val['status'] == "creator") {
-				$creator = "<a href=\"tg://user?id=".$val['user']['id']."\">".htmlspecialchars($name)."</a>".(isset($val['user']['username']) ? "@".$val['user']['username'] : "");
+				$creator = "<a href=\"tg://user?id=".$val['user']['id']."\">".htmlspecialchars($name)."</a>".(isset($val['user']['username']) ? " @".$val['user']['username'] : " (<code>no username</code>)");
 			} else {
 				$admin[] = "<a href=\"tg://user?id=".$val['user']['id']."\">".htmlspecialchars($name)."</a>".(isset($val['user']['username']) ? " @".$val['user']['username'] : " (<code>no username</code>)");
 			}

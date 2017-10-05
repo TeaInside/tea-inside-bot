9680c0288f51d70848f9db2587994c290d867f46
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-05 18:08:36 +0700

    Init start cmd

---
+++ src/Handler/Response/NormalMessage/Command/Start.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Handler\Response\NormalMessage\Command;
+
+use Lang;
+use Telegram as B;
+use Handler\MainHandler;
+use Handler\Response\Foundation\CommandFactory;
+
+class Translate extends CommandFactory
+{
+	/**
+	 * @var Handler\MainHandler
+	 */
+	private $h;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Handler\MainHandler
+	 */
+	public function __construct(MainHandler $handler)
+	{
+		$this->h = $handler;
+	}
+
+	/**
+	 * Run.
+	 */
+	public function __run()
+	{
+		B::sendMessage(
+			[
+				"chat_id" => $this->h->chat_id,
+				"text"    => Lang::system("/start"),
+				"reply_to_message_id" => $this->h->msgid
+			]
+		);
+	}
+}

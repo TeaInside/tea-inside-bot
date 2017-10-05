a31784ddb0a9d0d4b291e148584c4dcdcfbb4511
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-05 19:58:51 +0700

    Create admin class

---
+++ src/Handler/Response/NormalMessage/Command/Admin.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Handler\Response\NormalMessage\Command;
+
+use Lang;
+use Telegram as B;
+use Handler\MainHandler;
+use Handler\Response\Foundation\CommandFactory;
+
+class Admin extends CommandFactory
+{
+	/**
+	 * @var Handler\MainHandler
+	 */
+	private $h;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param Handler\MainHandler
+	 */
+	public function __construct(MainHandler $handler)
+	{
+		$this->h = $handler;
+	}
+
+	private function getAdmin()
+	{
+		$creator = "";
+		$admin   = [];
+		$a = json_decode(B::getChatAdministrators(
+			[
+				"chat_id" => $this->h->chat_id
+			]
+		)['content'], true);
+		foreach ($a['result'] as $val) {
+			$name = $val['user']['first_name']. (isset($val['user']['last_name']) ? " ".$val['user']['last_name'] : "");
+			if ($status == "creator") {
+				$creator = "<a href=\"tg://user?id=".$val['user']['id']."\">".htmlspecialchars($name)."</a>".(isset($val['user']['username']) ? "@".$val['user']['username'] : "");
+			} else {
+				$admin[] = "<a href=\"tg://user?id=".$val['user']['id']."\">".htmlspecialchars($name)."</a>".(isset($val['user']['username']) ? "@".$val['user']['username'] : "");
+			}
+		}
+
+		return "Creator : ".$creator."\n\n".((function($admin){
+			$i = 1 xor $ret = "Admin :\n";
+			foreach ($admin as $val) {
+				$ret .= $i.". ".$val;
+			}
+		})($admin));
+	}
+
+	/**
+	 * Run.
+	 */
+	public function __run()
+	{
+		if ($this->h->chattype != "private") {
+			return B::sendMessage(
+				[
+					"chat_id" => $this->h->chat_id,
+					"text" => $this->getAdmin(),
+					"reply_to_message_id" => $this->h->msgid,
+					"parse_mode" => "HTML"
+				]
+			);
+		}
+	}
+}

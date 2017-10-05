eeeccd5282802c5ea353c55098520892fe3bc50f
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-05 19:40:43 +0700

    Class Adaptator

--- src/Handler/Response/NormalMessage/Command/TranslateRepliedMessage.php
+++ src/Handler/Response/NormalMessage/Command/TranslateRepliedMessage.php
@@ -20,14 +20,22 @@ class TranslateRepliedMessage extends CommandFactory
 	{
 		if (isset($this->h->replyto['text'])) {
 			$x = explode(" ", $this->h->text, 4);
-			$from = $x[1];
-			$to   = $x[2];
+			if (isset($x[1], $x[2])) {
+				$from = $x[1];
+				$to   = $x[2];
+			} elseif (isset($x[1])) {
+				$from = $x[1];
+				$to   = "id";
+			} else {
+				$from = "auto";
+				$to   = "id";
+			}
 			$st = new GoogleTranslate($this->h->replyto['text'], $from, $to);
 			B::sendMessage(
 				[
 					"text" => $st->exec(),
 					"chat_id" => $this->h->chat_id,
-					"reply_to_message_id" => $this->h->msgid,
+					"reply_to_message_id" => $this->h->replyto['message_id'],
 					"disable_web_page_preview" => true
 				]
 			);

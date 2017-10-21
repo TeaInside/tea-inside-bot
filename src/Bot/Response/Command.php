96b1fdba6a8aed37c00758e737b91bf9ae938924
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-21 22:04:38 +0700

    Update Command class

--- src/Bot/Response/Command.php
+++ src/Bot/Response/Command.php
@@ -51,7 +51,7 @@ final class Command
 					$a = explode("@", $val[1]);
 					$a[0] = "Bot\\Response\\Command\\".$a[0];
 					$st = new $a[0]($this->b);
-					$st->{$a[1]}();
+					// $st->{$a[1]}();
 				}
 				return true;
 			}

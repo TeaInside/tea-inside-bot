82baeb1d48bbf39130978dcb5a5fa018241d4113
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-21 22:50:14 +0700

    Update FakeWebhookTest

--- tests/FakeWebhookTest/DirectTest.php
+++ tests/FakeWebhookTest/DirectTest.php
@@ -5,29 +5,39 @@ namespace FakeWebhookTest;
 use Bot\Bot;
 use PHPUnit\Framework\TestCase;
 
-class DirectTest extends TestCase
+class DirectChatTest extends TestCase
 {
 	public function testFake1()
 	{
 		$json = '{
-    "message_id": 3461593,
-    "from": {
-        "id": 243692601,
-        "is_bot": false,
-        "first_name": "Ammar",
-        "last_name": "F",
-        "username": "ammarfaizi2",
-        "language_code": "en-US"
-    },
-    "chat": {
-        "id": 243692601,
-        "first_name": "Ammar",
-        "last_name": "F",
-        "username": "ammarfaizi2",
-        "type": "private"
-    },
-    "date": 1508600597,
-    "text": "/sh Test"
+    "update_id": 344317636,
+    "message": {
+        "message_id": 23062,
+        "from": {
+            "id": 243692601,
+            "is_bot": false,
+            "first_name": "Ammar",
+            "last_name": "F",
+            "username": "ammarfaizi2",
+            "language_code": "en-US"
+        },
+        "chat": {
+            "id": 243692601,
+            "first_name": "Ammar",
+            "last_name": "F",
+            "username": "ammarfaizi2",
+            "type": "private"
+        },
+        "date": 1508600922,
+        "text": "\/sh echo \"direct test (sh)\"",
+        "entities": [
+            {
+                "offset": 0,
+                "length": 3,
+                "type": "bot_command"
+            }
+        ]
+    }
 }';
 		$bot = new Bot($json);
 		$this->assertTrue($bot->run());
--- tests/FakeWebhookTest/ServerTest.php
+++ tests/FakeWebhookTest/ServerTest.php
@@ -13,24 +13,34 @@ class ServerTest extends TestCase
 		shell_exec(PHP_BINARY." -S 127.0.0.1:9000 -t ".BASEPATH."/public >> /dev/null 2>&1 &");
 		$ch = new Curl("http://127.0.0.1:9000/webhook.php");
 		$ch->post('{
-    "message_id": 3461593,
-    "from": {
-        "id": 243692601,
-        "is_bot": false,
-        "first_name": "Ammar",
-        "last_name": "F",
-        "username": "ammarfaizi2",
-        "language_code": "en-US"
-    },
-    "chat": {
-        "id": 243692601,
-        "first_name": "Ammar",
-        "last_name": "F",
-        "username": "ammarfaizi2",
-        "type": "private"
-    },
-    "date": 1508600597,
-    "text": "qwe"
+    "update_id": 344317636,
+    "message": {
+        "message_id": 23062,
+        "from": {
+            "id": 243692601,
+            "is_bot": false,
+            "first_name": "Ammar",
+            "last_name": "F",
+            "username": "ammarfaizi2",
+            "language_code": "en-US"
+        },
+        "chat": {
+            "id": 243692601,
+            "first_name": "Ammar",
+            "last_name": "F",
+            "username": "ammarfaizi2",
+            "type": "private"
+        },
+        "date": 1508600922,
+        "text": "\/sh echo \"server test (sh)\"",
+        "entities": [
+            {
+                "offset": 0,
+                "length": 3,
+                "type": "bot_command"
+            }
+        ]
+    }
 }');
 		$out = $ch->exec();
         var_dump($out);

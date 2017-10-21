6609c1a2698fbd593ee315c579e9b4ae784f380b
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-21 21:16:26 +0700

    Init Lang class

---
+++ src/Lang.php
@@ -0,0 +1,38 @@
+<?php
+
+use System\Hub\Singleton;
+
+final class Lang
+{
+	use Singleton;
+
+	/**
+	 * @var string
+	 */
+	private $lang;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param string $lang
+	 */
+	public function __construct($lang)
+	{
+		$this->lang = $lang;
+	}
+
+	/**
+	 * @param string $lang
+	 * @return bool
+	 */
+	public static function init($lang)
+	{
+		self::getInstance($lang);
+		return true;
+	}
+
+	public function get($space)
+	{
+
+	}
+}
---
+++ src/System/Hub/Singleton.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace System\Hub;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ */
+trait Singleton
+{	
+	/**
+	 * @var self
+	 */
+	private static $instance;
+
+	/**
+	 * Get instance.
+	 */
+	public function getInstance()
+	{
+		if (self::$instance === null) {
+			self::$instance = new self(...func_get_args());
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Prevent cloning instance.
+	 */
+	final private function __clone()
+	{
+	}
+
+	/**
+	 * Prevent sleep.
+	 */
+	final private function __sleep()
+	{
+	}
+}

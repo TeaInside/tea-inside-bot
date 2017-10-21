744f15928f70b3e1da002a14d95aa4075c439814
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-21 22:07:46 +0700

    Prepare ping route

--- src/Bot/Response/CommandRoutes.php
+++ src/Bot/Response/CommandRoutes.php
@@ -8,6 +8,36 @@ trait CommandRoutes
 	{
 		$this->set(function()
 		{
+			$st = explode(" ", $this->b->lowertext, 2);
+			return 
+				$st[0] === "/sh"  	 ||
+				$st[0] === "!sh" 	 ||
+				$st[0] === "~sh" 	 ||
+				$st[0] === "sh"  	 ||
+				$st[0] === "/shexec" || 
+				$st[0] === "!shexec" ||
+				$st[0] === "~shexec" ||
+				$st[0] === "shexec";
 		}, "ShellExec@run");
+
+		$this->set(function()
+		{
+			$st = explode(" ", $this->b->lowertext, 2);
+			$st = explode("@", $st[0]);
+			return 
+				$st[0] === "/ping"   ||
+				$st[0] === "!ping" 	 ||
+				$st[0] === "~ping" 	 ||
+				$st[0] === "ping";
+		}, function(){
+			$start = microtime(true);
+			$st = json_decode(B::sendMessage(
+				[
+					"text" 	  	          => "Pong!",
+					"chat_id" 			  => $this->b->chat_id,
+					"reply_to_message_id" => $this->b->msgid
+				]
+			)['content']);
+		});
 	}
 }

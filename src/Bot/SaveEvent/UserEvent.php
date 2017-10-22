ac6bffb293265225ccb47fb77027da0cd3b18c1d
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-22 17:31:34 +0700

    Fix update user info

--- src/Bot/SaveEvent/UserEvent.php
+++ src/Bot/SaveEvent/UserEvent.php
@@ -20,6 +20,11 @@ class UserEvent extends EventFoundation
 	private $b;
 
 	/**
+	 * @var array
+	 */
+	private $oldIdentity = [];
+
+	/**
 	 * Constructor.
 	 *
 	 * @param \Bot\Bot $bot
@@ -40,11 +45,11 @@ class UserEvent extends EventFoundation
 		if (! $st) {
 			return false;
 		}
-
 		if (
 			$st['username'] !== $this->b->username ||
 			$st['name']     !== $this->b->name
 		) {
+			$this->oldIdentity = $st;
 			return "update";
 		}
 		return "known";
@@ -56,7 +61,7 @@ class UserEvent extends EventFoundation
 	private function updateUserInfo()
 	{
 		$add = $this->b->chattype === "private" ? "`private_msg_count`=`private_msg_count`+1" : "`group_msg_count`=`group_msg_count`+1";
-		$st = DB::preapre("UPDATE `a_users` SET `username`=:username, `name`=:name, `photo` = NULL, `updated_at`=:updated_at, {$add} WHERE `user_id`=:user_id LIMIT 1;");
+		$st = DB::prepare("UPDATE `a_users` SET `username`=:username, `name`=:name, `photo` = NULL, `updated_at`=:updated_at, {$add} WHERE `user_id`=:user_id LIMIT 1;");
 		pc($st->execute(
 			[
 				":username" 	=> $this->b->username,
@@ -65,8 +70,35 @@ class UserEvent extends EventFoundation
 				":user_id" 		=> $this->b->user_id
 			]
 		), $st);
+		$this->b->chattype !== "private" and $this->notify();
 		$this->writeUserHistory();
 		return true;
+	}
+
+	private function notify()
+	{
+		$st = DB::prepare("SELECT `history_notification` FROM `groups_setting` WHERE `group_id`=:group_id LIMIT 1;");
+		pc($st->execute([":group_id" => $this->b->chat_id]), $st);
+		if ($st = $st->fetch(PDO::FETCH_NUM)) {
+			if ($st[0] === 'true') {
+				if ($this->b->name !== $this->oldIdentity['name']) {
+					B::sendMessage(
+						[
+							"text" =>  $this->oldIdentity['name']." changes name to ".$this->b->name,
+							"chat_id" => $this->b->chat_id
+						]
+					);
+				}
+				if ($this->b->username !== $this->oldIdentity['username']) {
+					B::sendMessage(
+						[
+							"text" =>  $this->oldIdentity['name']." changes username from ".$this->oldIdentity['username']." to ".$this->b->username,
+							"chat_id" => $this->b->chat_id
+						]
+					);
+				}
+			}
+		}
 	}
 
 	/**

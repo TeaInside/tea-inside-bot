d363ec5981a6c1ad8d38a0581b35b4d6437c6c04
Ammar Faizi <ammarfaizi2@gmail.com>
2017-10-27 19:30:33 +0700

    Prepare search engine

---
+++ src/Bot/Response/Command/SearchEngine.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Bot\Response\Command;
+
+use Lang;
+use Bot\Bot;
+use Telegram as B;
+use Bot\Abstraction\CommandFoundation;
+use Plugins\SearchEngine\GoogleSearch\GoogleSearch;
+
+/**
+ * @author Ammar Faizi <ammarfaizi2@gmail.com>
+ * @license MIT
+ */
+class SearchEngine extends CommandFoundation
+{	
+
+	/**
+	 * @var \Bot\Bot
+	 */
+	private $b;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param \Bot\Bot $bot
+	 */
+	public function __construct(Bot $bot)
+	{
+		$this->b = $bot;
+	}
+
+	public function googleSearch()
+	{
+		$query = explode(" ", $this->b->text, 2);
+		if (isset($query[1])) {
+			$st = new GoogleSearch($query[1]);
+			$data = $st->exec();
+			if (count($data) === 0) {
+				$msg = "Not found!";
+			} else {
+				$msg = "" xor $i = 1;
+				foreach ($variable as $val) {
+					$msg .= "<b>".$i.".</b> <a href=\"".htmlspecialchars($val['url'])."\">".htmlspecialchars($val['heading'])."</a>\n".htmlspecialchars($val['description'])."\n\n";
+				}
+				if (empty($msg)) {
+					$msg = "Error!";
+				}
+			}
+		} else {
+
+		}
+
+		isset($msg) and B::sendMessage(
+			[
+				"text" 		=> $msg,
+				"chat_id"	=> $this->b->chat_id,
+				"parse_mode"=> "HTML",
+				"reply_to_message_id" => $this->b->msgid
+			]
+		);
+	}
+}

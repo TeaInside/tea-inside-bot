57136f71873f855f74a20625d2149de384a1dedd
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-01 15:42:53 +0700

    Create init class

---
+++ src/Plugins/KulgramWriter/Actions/Init.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Plugins\KulgramWriter\Actions;
+
+use Telegram as B;
+use Plugins\KulgramWriter\KulgramWriterFoundation;
+
+class Init extends KulgramWriterFoundation
+{
+	public function run()
+	{
+		$l = $this->b->lowertext;
+		$a = explode("topik saat ini", $l, 2);
+		if (! isset($a[1])) {
+			$a = explode("topik hari ini", $l, 2);
+			if (! isset($a[1])) {
+				$a = explode("materi hari ini", $l, 2);
+				if (! isset($a[1])) {
+					$a = explode("materi kulgram hari ini", $l, 2);
+					if (! isset($a[1])) {
+						$a = explode("topik kulgram hari ini", $l, 2);
+						if (! isset($a[1])) {
+							return false;
+						}
+					}
+				}
+			}
+		}
+		$b = explode("oleh", $a[1], 2);
+		$author = isset($b[1]) ? ucwords(strtolower($b[1])) : $this->b->name;
+		$title  = strtoupper(trim($a[0]));
+		file_put_contents($this->lockfile, data)
+		B::sendMessage(
+			[
+				"text" => "Baiklah, materi kulgram hari ini adalah \"<b>".htmlspecialchars($title)."</b>\" oleh <b>".htmlspecialchars($author)."</b>",
+				"chat_id" => $this->b->chat_id,
+				"parse_mode" => "HTML"
+			]
+		);
+	}
+}

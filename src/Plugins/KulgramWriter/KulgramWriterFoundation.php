8added8d46e198fd7469b6791e447475ed1edcfe
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-01 15:42:42 +0700

    Create getInfo method

--- src/Plugins/KulgramWriter/KulgramWriterFoundation.php
+++ src/Plugins/KulgramWriter/KulgramWriterFoundation.php
@@ -12,19 +12,30 @@ abstract class KulgramWriterFoundation
 	
 	protected $lockfile;
 
+	protected $infofile;
+
 	protected $tmpdir;
+
+	protected $infodata = [];
 	
 	public function __construct(Bot $bot)
 	{
 		$this->b = $bot;
 		$this->path = data."/kulgram";
 		$this->lockfile = $this->path."/data/".$bot->chat_id."/lock/".$bot->chat_id.".lock";
-		$this->info = $this->path."/info/".$bot->chat_id.".info";
+		$this->infofile = $this->path."/info/".$bot->chat_id.".info";
 		$this->tmpdir = $this->path."/data/".$bot->chat_id."/tmp/";
 		is_dir($this->path) or mkdir($this->path);
 		is_dir($this->path."/data") or mkdir($this->path."/data");
 		is_dir($this->path."/info") or mkdir($this->path."/info");
 		is_dir($this->path."/data/".$bot->chat_id."/lock") or mkdir($this->path."/data/".$bot->chat_id."/lock");
 		is_dir($this->path."/data/".$bot->chat_id."/tmp") or mkdir($this->path."/data/".$bot->chat_id."/tmp");
+	}
+
+	protected function getInfo()
+	{
+		if (file_exists($this->infofile)) {
+			$this->infodata = json_decode(file_get_contents($this->infofile), true);
+		}
 	}
 }

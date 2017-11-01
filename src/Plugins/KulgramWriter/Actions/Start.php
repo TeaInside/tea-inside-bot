73890e59ec21cec6ac97528abe2d569d37283359
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-01 15:03:21 +0700

    Create new KulgramWriter concept

---
+++ src/Plugins/KulgramWriter/Actions/Start.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Plugins\KulgramWriter\Actions;
+
+use Telegram as B;
+use Plugins\KulgramWriter\KulgramWriterFoundation;
+
+class Start extends KulgramWriterFoundation
+{
+	public function run()
+	{
+		if (file_exists($this->lockfile)) {
+			return false;
+		}
+		if (file_exists($this->initfile)) {
+			B::sendMessages(
+				[
+					
+				]
+			);
+		}
+	}
+}
--- src/Plugins/KulgramWriter/KulgramWriterFoundation.php
+++ src/Plugins/KulgramWriter/KulgramWriterFoundation.php
@@ -6,8 +6,29 @@ use Bot\Bot;
 
 abstract class KulgramWriterFoundation
 {
+	protected $b;
+	
+	protected $path;
+	
+	protected $lockfile;
+	
+	protected $datafile;
+	
+	protected $infofile;
+	
+	protected $initfile;
+	
 	public function __construct(Bot $bot)
 	{
-		
+		$this->b = $bot;
+		$this->path = data."/kulgram/";
+		$this->lockfile = $this->path."/lock/".$bot->chat_id.".lock";
+		$this->datafile = $this->path."/data/".$bot->chat_id.".data";
+		$this->infofile = $this->path."/info/".$bot->chat_id.".info";
+		$this->initfile = $this->path."/init/".$bot->chat_id;
+		is_dir($a=$this->path."/lock") or mkdir($a);
+		is_dir($a=$this->path."/data") or mkdir($a);
+		is_dir($a=$this->path."/info") or mkdir($a);
+		is_dir($a=$this->path."/init") or mkdir($a);
 	}
 }

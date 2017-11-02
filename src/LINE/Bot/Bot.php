6f617810dd31298aa8c05285b5f25bebf55422f3
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-02 16:05:58 +0700

    Preapre final class Bot for LINE Stack

--- src/LINE/Bot/Bot.php
+++ src/LINE/Bot/Bot.php
@@ -1,3 +1,25 @@
 <?php
 
 namespace LINE\Bot;
+
+final class Bot
+{
+
+	/**
+	 * @var array
+	 */
+	public $input = [];
+
+	/**
+	 * @param string $data
+	 */
+	public function __construct($data)
+	{
+		$this->input = json_decode($data, true);
+	}
+
+	public function run()
+	{
+		
+	}
+}

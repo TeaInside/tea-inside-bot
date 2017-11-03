08a2c77478ebed06f28fee1bc9de7cf048d0fd1e
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-03 23:01:44 +0700

    Update route

--- src/LINE/Bot/Response/CommandRoutes.php
+++ src/LINE/Bot/Response/CommandRoutes.php
@@ -21,30 +21,7 @@ trait CommandRoutes
                 $st[0] = strtolower($st[0]);
                 return $st[0] === "/google" or $st[0] === "/g";
             },
-            function () use ($st) {
-                $st = new \Plugins\SearchEngine\GoogleSearch\GoogleSearch($st[1]);
-                $st = $st->exec();
-                $r = "";
-                $i=1;
-                foreach ($st as $z) {
-                    $r.= ($i++).". ".$z['heading']."\n".$z['url']."\n".$z['description']."\n\n";
-                }
-                var_dump($r);
-                $r = trim($r);
-                foreach (str_split($r, 1999) as $q) {
-                    $msg[] = [
-                    "type" => "text",
-                    "text" => $q
-                    ];
-                }
-                $r = empty($r)?"Not found!":$r;
-                print LINE::push(
-                    [
-                    "to" => $this->b->chat_id,
-                    "messages" => $msg
-                    ]
-                )['content'];
-            }
+            "SearchEngine@googleSearch"
         );
         
         $this->route(

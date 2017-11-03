2372b5492131f849b9655a9567279acbf583bce7
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-03 10:09:47 +0700

    Fix route

--- src/LINE/Bot/Response/CommandRoutes.php
+++ src/LINE/Bot/Response/CommandRoutes.php
@@ -11,6 +11,7 @@ trait CommandRoutes
 		$st = explode(" ", $this->b->text, 2);
 		
 		$this->route(function() use ($st){
+			var_dump($st);
 			return $st[0] === "/google";
 		}, function() use ($st) {
 			$st = new \Plugins\SearchEngine\GoogleSearch\GoogleSearch($st[1]);

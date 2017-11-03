337f0f864479770d6c9f4afe1389ef698162f676
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-03 18:08:23 +0700

    PSR-2 Coding standard fixer

--- tests/Telegram/FakeWebhook/PrivateTest.php
+++ tests/Telegram/FakeWebhook/PrivateTest.php
@@ -7,56 +7,56 @@ use PHPUnit\Framework\TestCase;
 
 class PrivateTest extends TestCase
 {
-	/**
-	 * Constructor.
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-		require_once BASEPATH."/config/telegram.php";
-	}
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        require_once BASEPATH."/config/telegram.php";
+    }
 
-	/**
-	 * Test webhook 1
-	 */
-	public function testFakeWebhook()
-	{
-		$json = json_encode(
-			[
-				"update_id" => rand(10000000, 99999999),
-				"message" => [
-					"message_id" => rand(1, 100),
-					"from" => [
-						"id" 		 	=> 243692601,
-						"is_bot"     	=> false,
-						"first_name" 	=> "Ammar",
-						"last_name"  	=> "Faizi",
-						"username"	 	=> "ammarfaizi2",
-						"language_code"	=> "en-US"
-					],
-					"chat" => [
-						"id"		=> -1001134449138,
-						"first_name"=> "Ammar",
-						"last_name"	=> "F.",
-						"username"	=> "ammarfaizi2",
-						"type"		=> "private"
-					],
-					"date" => time(),
-					"text" => "ping"
-				]
-			]
-		);
+    /**
+     * Test webhook 1
+     */
+    public function testFakeWebhook()
+    {
+        $json = json_encode(
+            [
+                "update_id" => rand(10000000, 99999999),
+                "message" => [
+                    "message_id" => rand(1, 100),
+                    "from" => [
+                        "id"            => 243692601,
+                        "is_bot"        => false,
+                        "first_name"    => "Ammar",
+                        "last_name"     => "Faizi",
+                        "username"      => "ammarfaizi2",
+                        "language_code"     => "en-US"
+                    ],
+                    "chat" => [
+                        "id"        => -1001134449138,
+                        "first_name"=> "Ammar",
+                        "last_name"     => "F.",
+                        "username"  => "ammarfaizi2",
+                        "type"      => "private"
+                    ],
+                    "date" => time(),
+                    "text" => "ping"
+                ]
+            ]
+        );
 
-		try {
-			$app = new Bot($json);
-			$app->run();
-			$action = true;
-		} catch (\Error $e) {
-			var_dump($e->getMessage());
-			$action = false;
-		}
-		$this->assertTrue($action);
-	}
+        try {
+            $app = new Bot($json);
+            $app->run();
+            $action = true;
+        } catch (\Error $e) {
+            var_dump($e->getMessage());
+            $action = false;
+        }
+        $this->assertTrue($action);
+    }
 }
 
 /*
--- tests/manual/circle.php
+++ tests/manual/circle.php
@@ -6,8 +6,8 @@ use Telegram as B;
 
 B::sendMessage(
     [
-        "text"		 => "CircleCI report : \n<b>Success! Your tests passed on CircleCI!</b>",
-        "chat_id"	 => -1001128531173,
+        "text"       => "CircleCI report : \n<b>Success! Your tests passed on CircleCI!</b>",
+        "chat_id"    => -1001128531173,
         "parse_mode" => "HTML"
     ]
 );

9f26b3ffa9b2f83928bdf1fb4d46f61dfdf968d1
Ammar Faizi <ammarfaizi2@gmail.com>
2017-11-06 13:39:03 +0700

    Create audio handler

--- src/Telegram/Bot/SaveEvent.php
+++ src/Telegram/Bot/SaveEvent.php
@@ -47,6 +47,9 @@ class SaveEvent
         case 'photo':
             $ns.="Photo";
             break;
+        case 'video':
+        			$ns.="Audio";
+       		   break;
         default:
             throw new \Exception("Unknown event", 1);
             break;
